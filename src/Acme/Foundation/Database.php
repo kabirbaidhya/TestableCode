9ba08bc24e0374a04196713586f42eae68cb76b1
Kabir <kabirbaidhya@lftechnology.com>
2015-11-04 09:36:16 +0545

    Add implementation of DatabaseInterface

---
+++ src/Acme/Foundation/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Acme\Foundation;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Query\QueryBuilder;
+
+/**
+ * @author Kabir Baidhya
+ */
+class Database implements DatabaseInterface
+{
+
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+    /**
+     * @param Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Creates and returns a new instance of DBAL QueryBuilder
+     *
+     * @return QueryBuilder
+     */
+    public function createQueryBuilder()
+    {
+        return $this->connection->createQueryBuilder();
+    }
+}
