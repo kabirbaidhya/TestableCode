0b08f176bc1bf44e5ccf383a87440345fa58f34e
Kabir <kabirbaidhya@lftechnology.com>
2015-11-04 09:52:21 +0545

    Add sample configuration file

---
+++ config/config.dist.php
@@ -0,0 +1,16 @@
+<?php
+// This is a sample configuration file.
+// Please make a copy of this file as 'config.php' and fill up your database config parameters
+//
+// Check this http://doctrine-orm.readthedocs.org/projects/doctrine-dbal/en/latest/reference/configuration.html#getting-a-connection
+// for more information about setting up Connection configuration
+//
+return [
+    'database' => [
+        'dbname' => 'DBNAME',
+        'user' => 'USER',
+        'password' => 'PASSWORD',
+        'host' => 'HOST',
+        'driver' => 'DRIVER',
+    ],
+];
