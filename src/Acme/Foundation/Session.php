832269130bc767d7c79730a85515896f71d732ee
Kabir <kabirbaidhya@lftechnology.com>
2015-11-13 18:37:54 +0545

    Add a database based Session implementation

---
+++ src/Acme/Foundation/Session.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Acme\Foundation;
+
+/**
+ * @author Kabir Baidhya
+ */
+class Session implements SessionInterface
+{
+
+    /**
+     * @var DatabaseInterface
+     */
+    private $db;
+
+    public function __construct(DatabaseInterface $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Gets the value of a key set in the session
+     *
+     * @param string $key
+     * @return string|null
+     */
+    public function get($key)
+    {
+        $query = $this->db->createQueryBuilder();
+
+        $value = $query
+            ->select('value')
+            ->from('sessions')
+            ->where($query->expr()->eq('key', ':key'))
+            ->setParameter(':key', $key)
+            ->execute()->fetchColumn();
+
+        return $value;
+    }
+
+    /**
+     * Checks if session has a key set
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        $query = $this->db->createQueryBuilder();
+
+        $value = $query
+            ->select('count(*)')
+            ->from('sessions')
+            ->where($query->expr()->eq('key', ':key'))
+            ->setParameter(':key', $key)
+            ->execute()->fetchColumn();
+
+        return (bool)$value;
+    }
+}
